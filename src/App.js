77f9ef6b28f285b36cb2fa71bed51a2d3a4f8096
Connor Rayburn <connorrayburn.work@gmail.com>
2021-04-23 17:37:33 -0700

    fixed some cypress tests

--- src/App.js
+++ src/App.js
@@ -94,7 +94,6 @@ const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
       <Route exact path='/'>
         <Home />
       </Route>
